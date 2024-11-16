cbd4a4ecd7c23bc04486fec6c6a39f12da1debe6
rm5973 <masurnaikrakshit@gmail.com>
2024-11-16 16:43:05 +0530

    new commit

--- src/main/java/registration.java
+++ src/main/java/registration.java
@@ -21,44 +21,6 @@ public class registration extends HttpServlet {
         String usernameEmail = req.getParameter("usernameEmail");
         String password = req.getParameter("password");
 
-<<<<<<< HEAD
-protected void service(HttpServletResponse res,HttpServletRequest req)throws IOException,ServletException, ClassNotFoundException, SQLException{
-
-String fname=req.getParameter("firstname");
-String lname=req.getParameter("lastname");
-String phone=req.getParameter("phone");
-String usernameEmail=req.getParameter("usernameEmail");
-String password=req.getParameter("password");
-
-
-
- boolean b=register(fname,lname,phone,usernameEmail,password);
- boolean l=login(usernameEmail, password);
-
-
-
-}
-
-protected boolean register(String fname,String lname,String phone,String usernameEmail,String password) throws ClassNotFoundException, SQLException{
-
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/register", "root", "root");
-    PreparedStatement p=con.prepareStatement("insert into registration ("+fname+","+lname+","+phone+","+usernameEmail+","+password+")");
-    return p.execute();
-    
-
-}
-
-  protected boolean login(String usernameEmail,String password) throws ClassNotFoundException, SQLException{
-    
-    Class.forName("com.mysql.cj.jdbc.Driver");
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/register", "root", "root");
-    PreparedStatement p=con.prepareStatement("insert into registration ("+usernameEmail+","+password+")");
-    return p.execute();
-    
-
-  }
-=======
         try {
             if (register(fname, lname, phone, usernameEmail, password)) {
                 // Redirect to login page if registration is successful
@@ -72,7 +34,6 @@ protected boolean register(String fname,String lname,String phone,String usernam
             res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred during registration");
         }
     }
->>>>>>> 37ea3e7548ce0d5dc3e2baaa164e1d7f9029d946
 
     private boolean register(String fname, String lname, String phone, String usernameEmail, String password) throws ClassNotFoundException, SQLException {
         // Load the MySQL driver
